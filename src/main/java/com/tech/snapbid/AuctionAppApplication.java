35034474bfa0a8c8d84e17478ee5cb5c0afe66d8
babafemiolatona <femiolatona@gmail.com>
2025-08-12 21:54:41 +0100

    feat: enable Spring data web support with DTO serialization mode

--- src/main/java/com/tech/snapbid/AuctionAppApplication.java
+++ src/main/java/com/tech/snapbid/AuctionAppApplication.java
@@ -2,13 +2,14 @@ package com.tech.snapbid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/")
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class AuctionAppApplication {
 
 	public static void main(String[] args) {
